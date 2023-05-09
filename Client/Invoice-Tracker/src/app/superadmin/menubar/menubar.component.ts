0d61ca7aad67c76786ab6fdc242277909a3ef280
ImArjunAsok <arjun14.asok@gmail.com>
2023-05-09 19:16:57 +0530

    Avatar and arrow commit

--- Client/Invoice-Tracker/src/app/superadmin/menubar/menubar.component.ts
+++ Client/Invoice-Tracker/src/app/superadmin/menubar/menubar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faArrowDown, faArrowUp } from '@fortawesome/free-solid-svg-icons';
 import { TokenHandler } from 'src/app/helpers/tokenHandler';
 
 @Component({
@@ -7,18 +8,26 @@ import { TokenHandler } from 'src/app/helpers/tokenHandler';
   styleUrls: ['./menubar.component.css']
 })
 export class MenubarComponent implements OnInit {
-
+  arrowUp = faArrowUp;
+  arrowDown = faArrowDown;
   name: string = '';
   role: string = '';
   email: string = '';
+  initials: string = '';
+  isHovered = false;
 
   ngOnInit(): void {
     this.name = this.tokenHandler.getNameFromToken();
     this.role = this.tokenHandler.getRoleFromToken();
     this.email = this.tokenHandler.getEmailFromToken();
+    this.initials = this.name.split(" ").map(m=>m.charAt(0)).join("");
   }
 
   constructor(private tokenHandler: TokenHandler) { }
+
+  rotateIcon(hovered: boolean) {
+    this.isHovered = hovered;
+  }
 
   profileDropdown() {
     var dropdown = document.querySelector('.dropdown-content') as HTMLElement | null;
