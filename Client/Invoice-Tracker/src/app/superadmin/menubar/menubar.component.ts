74b1369fdb50de0568a0e93a01a9517e1a4316b8
princegeorge121 <89679612+princegeorge121@users.noreply.github.com>
2023-05-11 18:22:41 +0530

    Commit changes

--- Client/Invoice-Tracker/src/app/superadmin/menubar/menubar.component.ts
+++ Client/Invoice-Tracker/src/app/superadmin/menubar/menubar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faAngleDown, faAngleUp, faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 import { TokenHandler } from 'src/app/helpers/tokenHandler';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-menubar',
@@ -29,7 +31,7 @@ export class MenubarComponent implements OnInit {
     this.initials = firstName.charAt(0) + lastName.charAt(0);
   }
 
-  constructor(private tokenHandler: TokenHandler) { }
+  constructor(private tokenHandler: TokenHandler, private router: Router) { }
 
   rotateIcon(hovered: boolean) {
     this.isHovered = hovered;
@@ -50,4 +52,32 @@ export class MenubarComponent implements OnInit {
       dropdown.classList.toggle('show');
     }
   }
+
+  handleLogOut() {
+
+    Swal.fire({
+
+      title: 'Are you sure?',
+
+      text: 'Do you want to log out?',
+
+      icon: 'warning',
+
+      showCancelButton: true,
+
+      confirmButtonText: 'Logout',
+
+      cancelButtonText: 'Cancel'
+
+    }).then((result: any) => {
+
+      if (result.isConfirmed) {
+
+        this.router.navigateByUrl('/');
+
+      }
+
+    });
+  }
 }
+
